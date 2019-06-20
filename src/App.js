feeae92245b6aa7c138313bf4930336f14e6458d
Kirill255 <24504648+Kirill255@users.noreply.github.com>
2019-06-20 19:05:16 +0300

    initial setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { render } from "react-dom";
+import Pet from "./Pet";
+
+const App = () => (
+  <div>
+    <h1>Adopt Me!</h1>
+    <Pet name="Luna" animal="dog" breed="Havanese" />
+    <Pet name="Pepper" animal="bird" breed="Cockatiel" />
+    <Pet name="Doink" animal="cat" breed="Mix" />
+  </div>
+);
+
+render(<App />, document.getElementById("root"));

bdf32c7d8c437ffd25155c489aaa8718f340aba9
Kirill255 <24504648+Kirill255@users.noreply.github.com>
2019-06-21 12:52:22 +0300

    add two html selects

--- src/Search.js
+++ src/Search.js
@@ -1,7 +1,11 @@
 import React, { useState } from "react";
+import { ANIMALS } from "@frontendmasters/pet"; // ANIMALS is array with pets
 
 const Search = () => {
   const [location, setLocation] = useState("Seattle, WA");
+  const [animal, setAnimal] = useState("dog"); // or try ""
+  const [breed, setBreed] = useState("");
+  const [breeds, setBreeds] = useState([]);
 
   return (
     <div className="search-params">
@@ -15,6 +19,45 @@ const Search = () => {
             onChange={e => setLocation(e.target.value)}
             placeholder="Location"
           />
+        </label>
+
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={e => setAnimal(e.target.value)}
+            onBlur={e => setAnimal(e.target.value)}
+          >
+            <option disabled key="Select animal" value="Select animal">
+              Select animal
+            </option>
+            {ANIMALS.map(animal => (
+              <option key={animal} value={animal}>
+                {animal}
+              </option>
+            ))}
+          </select>
+        </label>
+
+        <label htmlFor="breed">
+          Breed
+          <select
+            id="breed"
+            value={breed}
+            onChange={e => setBreed(e.target.value)}
+            onBlur={e => setBreed(e.target.value)}
+            disabled={!breeds.length}
+          >
+            <option disabled key="Select breeds" value="Select breeds">
+              Select breeds
+            </option>
+            {breeds.map(breed => (
+              <option key={breed} value={breed}>
+                {breed}
+              </option>
+            ))}
+          </select>
         </label>
         <button type="submit">Submit</button>
       </form>

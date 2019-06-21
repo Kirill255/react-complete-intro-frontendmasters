9d828d34ae04a11ca2935174495de8affb7ffb45
Kirill255 <24504648+Kirill255@users.noreply.github.com>
2019-06-21 15:18:48 +0300

    async calls to Petfinder API, update pet component

--- src/Pet.js
+++ src/Pet.js
@@ -1,11 +1,22 @@
 import React from "react";
 
-const Pet = ({ name, animal, breed }) => (
-  <div>
-    <h1>{name}</h1>
-    <h2>{animal}</h2>
-    <h2>{breed}</h2>
-  </div>
-);
+const Pet = ({ id, name, animal, breed, media, location }) => {
+  let hero = "http://placecorgi.com/300/300"; // placeholder, if the image is not exist
+  if (media.length) {
+    hero = media[0].small;
+  }
+
+  return (
+    <div className="pet">
+      <div className="image-container">
+        <img src={hero} alt={name} />
+      </div>
+      <div className="info">
+        <h1>{name}</h1>
+        <h2>{`${animal} — ${breed} — ${location}`}</h2>
+      </div>
+    </div>
+  );
+};
 
 export default Pet;
--- src/Search.js
+++ src/Search.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import pet, { ANIMALS } from "@frontendmasters/pet"; // ANIMALS is array with pets
+import Results from "./Results";
 import useDropdown from "./hooks/useDropdown";
 
 const Search = () => {
@@ -10,6 +11,17 @@ const Search = () => {
   // prettier-ignore
   const [animal, AnimalDropdown, setAnimal] = useDropdown("Animal", "dog", ANIMALS);
   const [breed, BreedDropdown, setBreed] = useDropdown("Breed", "", breeds);
+  const [pets, setPets] = useState([]);
+
+  async function requestPets() {
+    const { animals } = await pet.animals({
+      location,
+      breed,
+      type: animal
+    });
+
+    setPets(animals || []);
+  }
 
   useEffect(() => {
     // pet.breeds("dog").then(console.log, console.error); // get http://pets.dev-apis.com/types/dog/breeds
@@ -27,7 +39,12 @@ const Search = () => {
   return (
     <div className="search-params">
       <h2>{location || "Type location"}</h2>
-      <form>
+      <form
+        onSubmit={e => {
+          e.preventDefault();
+          requestPets();
+        }}
+      >
         <label htmlFor="location">
           Location
           <input
@@ -43,6 +60,8 @@ const Search = () => {
 
         <button type="submit">Submit</button>
       </form>
+
+      <Results pets={pets} />
     </div>
   );
 };

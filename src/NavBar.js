39a2cb5848d52a511a017cd090ad6c3c84a2f799
Kirill255 <24504648+Kirill255@users.noreply.github.com>
2019-08-13 12:08:24 +0300

    base usage emotion

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,22 +1,44 @@
-import React from "react";
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+import React, { useState } from "react";
 import { Link } from "@reach/router";
 import { css } from "@emotion/core";
+import colors from "./colors";
 
-const NavBar = () => (
-  <header
-    css={css`
-      position: sticky;
-      top: 0;
-      background-color: #333;
-      padding: 15px;
-      z-index: 10;
-    `}
-  >
-    <Link to="/">Adopt Me!</Link>
-    <span aria-label="logo" role="img">
-      🐩
-    </span>
-  </header>
-);
+const color = "deeppink";
+
+const NavBar = () => {
+  const [padding, setPadding] = useState(15);
+
+  return (
+    <header
+      onClick={() => setPadding(padding + 15)}
+      css={css`
+        position: sticky;
+        top: 0;
+        background-color: ${colors.dark};
+        padding: ${padding}px;
+        z-index: 10;
+      `}
+    >
+      <Link to="/">Adopt Me!</Link>
+      <span
+        css={css`
+          font-size: 60px;
+          color: ${color};
+
+          &:hover {
+            text-decoration: underline;
+            cursor: pointer;
+          }
+        `}
+        aria-label="logo"
+        role="img"
+      >
+        Logo 🐩
+      </span>
+    </header>
+  );
+};
 
 export default NavBar;

6c7abfc96225e7f817de70acc3e755df6519cb6b
Kirill255 <24504648+Kirill255@users.noreply.github.com>
2019-08-14 15:01:42 +0300

    code splitting libraries & child components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { useState, lazy, Suspense } from "react";
 import { render } from "react-dom";
 import { Router, Link } from "@reach/router";
-import Search from "./Search";
+// import Search from "./Search";
 // import Details from "./Details";
 import ThemeContext from "./ThemeContext";
 
 const Details = lazy(() => import("./Details"));
+const Search = lazy(() => import("./Search"));
 
 const App = () => {
   const themeHook = useState("darkblue"); // theme это [value, setValue] и мы сделали контекст с дэфолтным значением вида ["green", () => {}]

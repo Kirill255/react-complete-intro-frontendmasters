74010ac51ba8092442ab5b6182a7215a69e34934
Kirill255 <24504648+Kirill255@users.noreply.github.com>
2019-08-15 14:10:37 +0300

    typing the modal component v2

--- src/Modal.tsx
+++ src/Modal.tsx
@@ -1,21 +1,20 @@
 import React, { useEffect, useRef, FunctionComponent } from "react";
 import { createPortal } from "react-dom";
 
+/*
+We had to make it so the ref could never potentially be null by instantiating it inside the ref.
+Yes, this will create a new DOM node every time you render, and no that's probably not a big deal. We already know that React.Modal doesn't re-render very often.
+You can do it like we had been doing by using the type HTMLDivElement | null but then you have to null check anywhere you use elRef.current which is burdensome.
+*/
 const Modal: FunctionComponent = ({ children }) => {
-  const refEl = useRef<HTMLDivElement | null>(null);
-
-  if (!refEl.current) {
-    const div = document.createElement("div");
-    refEl.current = div;
-  }
+  const refEl = useRef(document.createElement("div"));
 
   useEffect(() => {
     const modalRoot = document.getElementById("modal");
-    if (!modalRoot || !refEl.current) return;
+    if (!modalRoot) return;
     modalRoot.appendChild(refEl.current);
 
     return () => {
-      if (!refEl.current) return;
       modalRoot.removeChild(refEl.current);
     };
   }, []);

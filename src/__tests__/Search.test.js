bb394316b1423aa94b3716850f95935fffce166c
Kirill255 <24504648+Kirill255@users.noreply.github.com>
2019-08-25 13:13:02 +0300

    add second test

--- src/__tests__/Search.test.js
+++ src/__tests__/Search.test.js
@@ -10,4 +10,8 @@ test("Search", async () => {
 
   const animalDropdown = getByTestId("use-dropdown-animal");
   expect(animalDropdown.children.length).toEqual(ANIMALS.length + 1);
+
+  expect(pet.breeds).toHaveBeenCalled(); // api was called
+  const breedDropdown = getByTestId("use-dropdown-breed");
+  expect(breedDropdown.children.length).toEqual(_breeds.length + 1); // and we get result
 });
